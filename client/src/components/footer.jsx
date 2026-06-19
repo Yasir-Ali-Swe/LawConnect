a34106f6861ae052cc87d58de686f5637c57d6e7
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-06-19 21:14:03 +0500

    Fix Footer Responsive ness

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   const year = date.slice(0, 4);
   return (
     <footer className="flex w-screen justify-center items-center bg-foreground text-background px-20 py-5">
-      <p className="text-background font-semibold">
+      <p className="text-background font-semibold text-center">
         &copy; {year} LawConnect. All rights reserved.
       </p>
     </footer>
