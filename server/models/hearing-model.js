5bbc713a8b9cb5355e6fa54dd06f54f98082d035
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-06-19 09:55:24 +0500

    feat: add hearing model

---
+++ server/models/hearing-model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const HearingSchema = new mongoose.Schema(
+  {
+    caseId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Case",
+      required: true,
+    },
+
+    date: {
+      type: Date,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["scheduled", "adjourned", "completed"],
+      default: "scheduled",
+    },
+
+    remarks: {
+      type: String,
+      required: true,
+    },
+
+    updatedReason: {
+      type: String,
+      default: null,
+    },
+
+    updatedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    updatedByRole: {
+      type: String,
+      enum: ["clerk", "court_officer"],
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Hearing", HearingSchema);
