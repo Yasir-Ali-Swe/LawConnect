2f63954646d6885b378cb5bfde166698bd281063
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-06-19 09:55:43 +0500

    feat: add user info model

---
+++ server/models/user-info-model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userProfileSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    dob: {
+      type: Date,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    province: {
+      type: String,
+      required: true,
+      enum: ["Punjab", "Sindh", "KPK", "Balochistan"],
+    },
+    profileImageUrl: {
+      type: String,
+      default: null,
+    },
+    profileImagePublicId: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true },
+);
+
+const UserProfileModel = mongoose.model("UserInfo", userProfileSchema);
+
+export default UserProfileModel;
