ffa2bb5fec4ffd7f87822b7d5b0253896569b40f
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-06-19 09:46:39 +0500

    feat: add footer component

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  const date = new Date().toISOString();
+  const year = date.slice(0, 4);
+  return (
+    <footer className="flex w-screen justify-center items-center bg-foreground text-background px-20 py-5">
+      <p className="text-background font-semibold">
+        &copy; {year} LawConnect. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
