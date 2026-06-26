ef8fc52bec86382a5d6c7f47e7bed5ed5802cf15
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-06-26 12:45:23 +0500

    update admin dashboard sidebar links

--- client/src/config/sidebar/admin.js
+++ client/src/config/sidebar/admin.js
@@ -3,30 +3,30 @@ import {
   Building2,
   UserCog,
   Users,
-  UserPen
+  UserPen,
 } from "lucide-react";
 
 export const adminSidebarItems = [
   {
     title: "Dashboard",
     url: "/dashboard/admin",
-    icon: LayoutDashboard,
+    icon: <LayoutDashboard className="mr-2 h-5 w-5" />,
     exact: true,
   },
   {
     title: "Courts",
     url: "/dashboard/admin/courts",
-    icon: Building2,
+    icon: <Building2 className="mr-2 h-5 w-5" />,
   },
   {
     title: "Users",
     url: "/dashboard/admin/users",
-    icon: Users,
+    icon: <Users className="mr-2 h-5 w-5" />,
   },
 
   {
     title: "Profile",
     url: "/dashboard/admin/profile",
-    icon: UserPen,
+    icon: <UserPen className="mr-2 h-5 w-5" />,
   },
 ];
