7ba69d1fb6ed2e5681ed2b19ecf57014e254701e
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-06-26 12:37:52 +0500

    Fix Dashboard sidebar

--- client/src/config/sidebar/client.js
+++ client/src/config/sidebar/client.js
@@ -11,32 +11,32 @@ export const clientSidebarItems = [
   {
     title: "Dashboard",
     url: "/dashboard/client",
-    icon: <LayoutDashboard className="mr-2 h-4 w-4" />,
+    icon: <LayoutDashboard className="mr-2 h-5 w-5" />,
     exact: true,
   },
   {
     title: "Cases",
     url: "/dashboard/client/cases",
-    icon: <Gavel className="mr-2 h-4 w-4" />,
+    icon: <Gavel className="mr-2 h-5 w-5" />,
   },
   {
     title: "Proposals",
     url: "/dashboard/client/proposals",
-    icon: <HandHelping className="mr-2 h-4 w-4" />,
+    icon: <HandHelping className="mr-2 h-5 w-5" />,
   },
   {
     title: "Messages",
     url: "/dashboard/client/messages",
-    icon: <MessageCircle className="mr-2 h-4 w-4" />,
+    icon: <MessageCircle className="mr-2 h-5 w-5" />,
   },
   {
     title: "Notifications",
     url: "/dashboard/client/notifications",
-    icon: <Bell className="mr-2 h-4 w-4" />,
+    icon: <Bell className="mr-2 h-5 w-5" />,
   },
   {
     title: "Profile",
     url: "/dashboard/client/profile",
-    icon: <UserPen className="mr-2 h-4 w-4" />,
+    icon: <UserPen className="mr-2 h-5 w-5" />,
   },
 ];
